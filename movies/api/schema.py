9e2fe2dcfad0cbffbbd40ce733b1a617800ba9ef
pawlaczyk <pawlaczyk@g.pl>
2020-10-26 22:28:30 +0100

    Multiple models

--- movies/api/schema.py
+++ movies/api/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Movie
+from .models import Director, Movie
 
 
 class MovieType(DjangoObjectType): # podobnie do serializer
@@ -14,12 +14,22 @@ class MovieType(DjangoObjectType): # podobnie do serializer
         return "Old movie" if self.year < 2000 else "New movie"
 
 
+class DirectorType(DjangoObjectType): # podobnie do serializer
+    class Meta:
+        model = Director
+
+
 class Query(graphene.ObjectType):
     all_movies = graphene.List(MovieType)
     movie = graphene.Field(MovieType, id=graphene.Int(), title=graphene.String()) #aceptujemy parametr id typu Integer, tytul String
 
+    all_directors = graphene.List(DirectorType)
+
     def resolve_all_movies(self, info, **kwargs): # dodatkowe argumenty na pozniej
         return Movie.objects.all()
+
+    def resolve_all_directors(self, info, **kwargs): # dodatkowe argumenty na pozniej
+        return Director.objects.all()
 
     def resolve_movie(self, info, **kwargs): # dodatkowe argumenty na pozniej
         movie_id = kwargs.get('id')
